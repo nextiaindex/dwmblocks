52cee72bbdb8515b2445293432aba7260bc4af8b
NextiaIndex <nextiaindex@protonmail.com>
2025-08-03 05:44:35 +0700

    use seperator from my shell script and disable delimiter

--- blocks.def.h
+++ blocks.def.h
@@ -1,13 +1,17 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
-	/*Icon*/	/*Command*/			/*Update Interval*/	/*Update Signal*/
-	{"VOL ", "status-volume", 2, 10},
-	{"NET ", "status-network", 2, 0},
-	{"BRI ", "status-brightness", 2, 11},
-	{"BAT ", "status-battery",	2, 0},
-	{"UTC+7 ", "date '+%H:%M'", 5, 0},
+	/*Icon*/	/*Command*/				/*Update Interval*/		/*Update Signal*/
+	{"", 		"status-volume", 		2, 						10},
+	{"", 		"status-seperator",		2, 						0},
+	{"", 		"status-network", 		2, 						0},
+	{"", 		"status-seperator",		2, 						0},
+	{"",		"status-brightness",	2, 						11},
+	{"",		"status-seperator",		2, 						0},
+	{"",		"status-battery",		2, 						0},
+	{"",		"status-seperator",		2, 						0},
+	{"",		"status-clock", 		5, 						0},
 };
 
 //sets delimiter between status commands. NULL character ('\0') means no delimiter.
-static char delim[] = " | ";
+static char delim[] = "\0";
 static unsigned int delimLen = 5;
