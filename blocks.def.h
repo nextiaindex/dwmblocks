6830afd87707a513dd5ee048b74fd1d13681f655
NextiaIndex <nextiaindex@protonmail.com>
2025-08-02 12:07:03 +0700

    change the blocks

--- blocks.def.h
+++ blocks.def.h
@@ -1,9 +1,11 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
-	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
-
-	{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
+	/*Icon*/	/*Command*/			/*Update Interval*/	/*Update Signal*/
+	{"VOL ", "status-volume", 2, 10},
+	{"NET ", "status-network", 2, 0},
+	{"BRI ", "status-brightness", 2, 11},
+	{"BAT ", "status-battery",	2, 0},
+	{"UTC+7 ", "date '+%H:%M'", 5, 0},
 };
 
 //sets delimiter between status commands. NULL character ('\0') means no delimiter.
